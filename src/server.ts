434eb2ad80076af11f39f823cfcdd858285295ee
Morais-C <morais.carlos@hotmail.com>
2024-09-16 16:38:28 +0100

    Return {Users} again - to test the WebHook after a new push to repo

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ const prisma = new PrismaClient();
 app.get("/users", async () => {
   const users = await prisma.user.findMany();
 
-  return "test"//  { users };
+  return { users };
 });
 
 app.post("/users", async (req, res) => {
